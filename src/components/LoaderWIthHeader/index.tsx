d0a0e670a288675318a60f83503a38eeb4b5fc9e
bybogdan <bgdnvarlamov@gmail.com>
2023-05-03 23:29:40 +0400

    Set loader icon instead of text

--- src/components/LoaderWIthHeader/index.tsx
+++ src/components/LoaderWIthHeader/index.tsx
@@ -1,4 +1,6 @@
+import { twCenteringBlock } from "../../utils/twCommon";
 import { Header } from "../Header";
+import { Loader } from "../Loader";
 
 export const LoaderWithHeader = ({
   userName = "",
@@ -12,10 +14,11 @@ export const LoaderWithHeader = ({
   return (
     <div className="align-between flex min-h-screen flex-col gap-12 p-6 text-slate-900 dark:text-white">
       <Header userName={userName} userId={userId} homePageHref="/" />
-      <div className="flex min-w-full flex-col gap-8">
-        <h5 className="text-center text-xl font-semibold leading-tight text-gray-900 dark:text-white">
-          {message}
-        </h5>
+
+      <div className={`${twCenteringBlock} -mt-20`}>
+        <div className="flex flex-col gap-2">
+          <Loader />
+        </div>
       </div>
     </div>
   );
