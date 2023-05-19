80f35dfe44ba00ac91d554e1e09c23fed5477dc9
bybogdan <bgdnvarlamov@gmail.com>
2023-05-20 00:54:20 +0400

    Set black-translucen

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,7 +17,7 @@ class MyDocument extends Document {
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta
             name="apple-mobile-web-app-status-bar-style"
-            content="default"
+            content="black-translucen"
           />
           {/* apple-touch-icon */}
           <link
